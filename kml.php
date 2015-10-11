667643c7c223f01a91c3fe0f1fe341cb98f73abd
Linus Metzler <linus.metzler@gmail.com>
2015-10-11 15:57:45 +0200

    more style fixes

--- kml.php
+++ kml.php
@@ -2,9 +2,9 @@
 if (!empty($_POST)) {
     $angles = [];
     $angles[0] = mt_rand(0, 89); // get first angle, within 1st quadrant
-for ($i = 1; $i < 4; $i++) {
-    $angles[$i] = $angles[$i - 1] + 90;
-}
+    for ($i = 1; $i < 4; $i++) {
+        $angles[$i] = $angles[$i - 1] + 90;
+    }
 
     $directions = ['North', 'East', 'South', 'West'];
 
@@ -13,19 +13,19 @@ for ($i = 1; $i < 4; $i++) {
     $origin = ['lat' => trim($o[0]), 'lon' => trim($o[1])];
 
     $distance = 10; //km
-foreach ($angles as $i => $bearing) {
+    foreach ($angles as $i => $bearing) {
 
         //http://www.movable-type.co.uk/scripts/latlong.html
 
         $lat1 = deg2rad($origin['lat']);
-    $lon1 = deg2rad($origin['lon']);
-    $d = $distance / 6371;  // convert dist to angular distance in radians
+        $lon1 = deg2rad($origin['lon']);
+        $d = $distance / 6371;  // convert dist to angular distance in radians
         $b = deg2rad($bearing);
-    $lat2 = asin(sin($lat1) * cos($d) + cos($lat1) * sin($d) * cos($b));
-    $lon2 = $lon1 + atan2(sin($b) * sin($d) * cos($lat1), cos($d) - sin($lat1) * sin($lat2));
+        $lat2 = asin(sin($lat1) * cos($d) + cos($lat1) * sin($d) * cos($b));
+        $lon2 = $lon1 + atan2(sin($b) * sin($d) * cos($lat1), cos($d) - sin($lat1) * sin($lat2));
         //$lon2 = ($lon2+3*pi()) % (2*pi()) - pi();  // normalise to -180..+180º
         $points[$i] = ['lat' => rad2deg($lat2), 'lon' => rad2deg($lon2)];
-}
+    }
     $file = '<?xml version="1.0" encoding="UTF-8"?>
 <kml xmlns="http://www.opengis.net/kml/2.2">
 <Document>
